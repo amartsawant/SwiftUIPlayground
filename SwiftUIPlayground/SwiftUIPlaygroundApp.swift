24f818611ed4887f370826992a8512e63e849007
Amar Sawant <amar.t.sawant@gmail.com>
2020-11-30 23:10:48 +0530

    Initial Commit

---
+++ SwiftUIPlayground/SwiftUIPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPlaygroundApp.swift
+//  SwiftUIPlayground
+//
+//  Created by Amar Sawant on 30/11/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
